6c64ed5bf9dad8e786b452096f35bf94c22c6acf
Margaret <wanjirundirangu43@gmail.com>
2022-06-05 22:25:01 +0300

    Bigginers_website.github.io

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Spoke Vocational Training Centre Admission Form";
 document.querySelector("body").appendChild(h2);
+
+function tableValidation()
+var uname = document.registration.First_Name;
+var uname = document.registration.Second_Name;
+var uname = document.registration.Last_Name;
+var udate = document.registration.DATEOFBIRTH;
+var uname = document.registration.Email_Id;
+var uname = document.registration.Mobile_Num;
+var umale = document.registration.male;
+var ufemale = document.registration.female; if(userid_validation(uid,5,12))
+var uname = document.registration.Address;
+var uname = document.registration.City;
+var uname = document.registration.Courses;
